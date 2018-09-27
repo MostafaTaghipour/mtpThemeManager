23c80c419e6abb7d5dfdd6c0f08b42e9ecb1d35e
Mostafa Taghipour <mostafa.taghipour@ymail.com>
2018-09-27 19:43:46 +0330

    add night mode auto

--- mtpThemeManager/Classes/NightModeStatus.swift
+++ mtpThemeManager/Classes/NightModeStatus.swift
@@ -1,19 +1,18 @@
 //
 //  NightModeStatus.swift
-//  mtpThemeManager
+//  ThemeManager
 //
-//  Created by Mostafa Taghipour on 9/25/18.
+//  Created by Mostafa Taghipour on 11/3/17.
+//  Copyright © 2017 RainyDay. All rights reserved.
 //
 
 import Foundation
-
 
 public enum NightModeStatus {
     case disable
     case enable
     case auto
     
-   public static var all : [NightModeStatus] = [.disable , .enable , .auto]
+    public static var all : [NightModeStatus] = [.disable , .enable , .auto]
 }
-
 
